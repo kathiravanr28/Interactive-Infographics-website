b679c861e44da3eff65a0b3f188372c7ccb09a28
Kathir <kathiravanr280303@gmail.com>
2025-10-15 14:32:33 +0530

    ninth commit
    images component

---
+++ src/images/images.js
@@ -0,0 +1,71 @@
+// Main circles
+import videoandgame from "../assets/video-game&game-theory.jpg";
+import videoGame from "../assets/Video-Game.jpg";
+import gameTheory from "../assets/Game-Theory.jpg";
+
+// Video Game sub-types
+import actionAdventure from "../assets/Action-Adventure.jpg";
+import rolePlaying from "../assets/Role-Playing-Game.jpg";
+import shootingGame from "../assets/Shooting-Game.jpg";
+import strategyGame from "../assets/Strategy-Game.jpg";
+import sportsGame from "../assets/Sports-Game.jpg";
+import puzzleGame from "../assets/Puzzle-Game.jpg";
+
+// Game Theory sub-types
+import normalVsExtensive from "../assets/Normal&Extensive-Form.jpg";
+import perfectVsImperfect from "../assets/Perfect&Imperfect-Information.jpg";
+import simultaneousVsSequential from "../assets/Simultaneous&Sequential-Moves.jpg";
+import symmetricVsAsymmetric from "../assets/Symmetric&Asymmetric.jpg";
+import zeroSumVsNonZeroSum from "../assets/Zero-Sum&Non-Zero-Sum.jpg";
+import cooperativeVsNonCoop from "../assets/Cooperative&Non-Cooperative.png";
+
+//statistic image
+import ActionAdventurestat from "../assets/Action-Adventure-stat.jpg";
+import RolePlayingstat from "../assets/Role-Playing-Game-(RPG)-stat.jpg";
+import ShootingGamestat from "../assets/Shooting-Game-stat.jpg";
+import StrategyGamestat from "../assets/Strategy-Game-stat.jpg";
+import SportsGamestat from "../assets/Sports-Game-stat.jpg";
+import PuzzleGamestat from "../assets/Puzzle-Game-stat.jpg";
+import NormalVsExtensivestat from "../assets/Normal&Extensive-Form-stat.png";
+import PerfectVsImperfectstat from "../assets/Perfect&Imperfect-Information-stat.jpg";
+import SimultaneousVsSequentialstat from "../assets/Simultaneous&Sequential-Moves-stat.jpg";
+import SymmetricVsAsymmetricstat from "../assets/Symmetric&Asymmetric-stat.jpg";
+import ZeroSumVsNonZeroSumstat from "../assets/Zero-Sum&Non-Zero-Sum-game-stat.jpg";
+import CooperativeVsNonCoopstat from "../assets/Cooperative&Non-Cooperative-stat.jpg";
+
+export const IMAGES = {
+  // Main
+  videoandgame,
+  videoGame,
+  gameTheory,
+
+  // Video Game sub-types
+  actionAdventure,
+  rolePlaying,
+  shootingGame,
+  strategyGame,
+  sportsGame,
+  puzzleGame,
+
+  // Game Theory sub-types
+  normalVsExtensive,
+  perfectVsImperfect,
+  simultaneousVsSequential,
+  symmetricVsAsymmetric,
+  zeroSumVsNonZeroSum,
+  cooperativeVsNonCoop,
+
+  //statistic graph
+  ActionAdventurestat,
+  RolePlayingstat,
+  ShootingGamestat,
+  StrategyGamestat,
+  SportsGamestat,
+  PuzzleGamestat,
+  NormalVsExtensivestat,
+  PerfectVsImperfectstat,
+  SimultaneousVsSequentialstat,
+  SymmetricVsAsymmetricstat,
+  ZeroSumVsNonZeroSumstat,
+  CooperativeVsNonCoopstat
+};
