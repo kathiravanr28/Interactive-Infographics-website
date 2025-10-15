e34e4d4df918944c34de1700cda3125b89b4096f
Kathir <kathiravanr280303@gmail.com>
2025-10-15 14:28:19 +0530

    third commit
    video component

---
+++ src/videos/videos.js
@@ -0,0 +1,49 @@
+// videos.js
+// Main circles
+import videoandgame from "../assets/video-game&game-theory.webm";
+import videoGame from "../assets/Video-Game.webm";
+import gameTheory from "../assets/Game-Theory.webm";
+
+// Video Game sub-types
+import actionAdventure from "../assets/Action-Adventure.webm";
+import rolePlaying from "../assets/Role-Playing-Game.webm";
+import shootingGame from "../assets/Shooting-Game.webm";
+import strategyGame from "../assets/Strategy-Game.webm";
+import sportsGame from "../assets/Sports-Game.webm";
+import puzzleGame from "../assets/Puzzle-Game.webm";
+
+// Game Theory sub-types
+import normalVsExtensive from "../assets/Normal&Extensive-Form.webm";
+import perfectVsImperfect from "../assets/Perfect&Imperfect-Information.webm";
+import simultaneousVsSequential from "../assets/Simultaneous&Sequential-Moves.webm";
+import symmetricVsAsymmetric from "../assets/Symmetric&Asymmetric.webm";
+import zeroSumVsNonZeroSum from "../assets/Zero-Sum&Non-Zero-Sum.webm";
+import cooperativeVsNonCoop from "../assets/Cooperative&Non-Cooperative.webm";
+
+
+
+
+export const VIDEOS = {
+    // Main Circles
+    videoandgame,
+    videoGame,
+    gameTheory,
+
+  // Game Theory
+  cooperativeVsNonCoop,
+  zeroSumVsNonZeroSum,
+  symmetricVsAsymmetric,
+  simultaneousVsSequential,
+  perfectVsImperfect,
+  normalVsExtensive,
+
+  // Video Games
+  actionAdventure,
+  rolePlaying,
+  shootingGame,
+  strategyGame,
+  sportsGame,
+  puzzleGame,
+
+
+};
